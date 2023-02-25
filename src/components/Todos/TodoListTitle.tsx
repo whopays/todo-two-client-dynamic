f7ad448eab3aa87da6abd33e3d25eadcb679ab35
demns <demn_@tut.by>
2023-02-25 23:14:19 +0100

    Adds helmet for title

--- src/components/Todos/TodoListTitle.tsx
+++ src/components/Todos/TodoListTitle.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from '@apollo/client';
 import { CircularProgress, InputAdornment, TextField } from '@mui/material';
 import { useContext, useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
 import EDIT_TODO_LIST_TITLE from 'src/apollo/mutations/editTodoListTitle';
 import GET_TODOS from 'src/apollo/queries/getTodos';
 import { todoListTitleId } from 'src/config';
@@ -19,12 +20,6 @@ export default function TodoListTitle() {
     useMutation(EDIT_TODO_LIST_TITLE);
 
   useEffect(() => {
-    if (todoList?.title) {
-      document.title = `${todoList?.title} | Two do`;
-    }
-  }, [todoList?.title]);
-
-  useEffect(() => {
     if (innerValue !== todoList?.title && previousTitle !== todoList?.title) {
       setInnerValue(todoList?.title || '');
     }
@@ -40,31 +35,40 @@ export default function TodoListTitle() {
     });
   };
 
+  const title = todoList?.title ? `${todoList?.title} | Two do` : 'Two do';
+
   return (
-    <TextField
-      inputProps={{ 'data-cy': todoListTitleId, maxLength: 512 }}
-      disabled={isEditing}
-      fullWidth
-      placeholder="✏️ Title"
-      variant="standard"
-      value={innerValue}
-      onChange={(e) => {
-        setInnerValue(e.target.value);
-      }}
-      onBlur={submit}
-      error={!!editingError}
-      helperText={
-        !!editingError
-          ? `Something went wrong, potentially in internet connection; full error: ${editingError}`
-          : undefined
-      }
-      InputProps={{
-        endAdornment: (
-          <InputAdornment position="end">
-            {!!isEditing && <CircularProgress size={24} />}
-          </InputAdornment>
-        ),
-      }}
-    />
+    <>
+      <Helmet>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+      </Helmet>
+
+      <TextField
+        inputProps={{ 'data-cy': todoListTitleId, maxLength: 512 }}
+        disabled={isEditing}
+        fullWidth
+        placeholder="✏️ Title"
+        variant="standard"
+        value={innerValue}
+        onChange={(e) => {
+          setInnerValue(e.target.value);
+        }}
+        onBlur={submit}
+        error={!!editingError}
+        helperText={
+          !!editingError
+            ? `Something went wrong, potentially in internet connection; full error: ${editingError}`
+            : undefined
+        }
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {!!isEditing && <CircularProgress size={24} />}
+            </InputAdornment>
+          ),
+        }}
+      />
+    </>
   );
 }
