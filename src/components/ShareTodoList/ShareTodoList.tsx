cf1175718c822369abce3ed18060c2b30892072c
demns <demn_@tut.by>
2023-02-26 01:25:04 +0100

    updates shared nfo

--- src/components/ShareTodoList/ShareTodoList.tsx
+++ src/components/ShareTodoList/ShareTodoList.tsx
@@ -2,19 +2,18 @@ import ShareIcon from '@mui/icons-material/Share';
 import { IconButton } from '@mui/material';
 
 export default function ShareTodoList() {
-  const shareData = {
-    title: document.title,
-    text: document.title,
-    url: window.location.href,
-  };
-  console.log(shareData);
   return (
     <>
-      {!!navigator?.canShare?.(shareData) && (
+      {!!navigator?.canShare?.({ url: window.location.href }) && (
         <IconButton
           aria-label="share twodo list"
           onClick={async () => {
             try {
+              const shareData = {
+                title: document.title,
+                text: document.title,
+                url: window.location.href,
+              };
               await navigator.share(shareData);
             } catch (err) {
               console.log(err);
