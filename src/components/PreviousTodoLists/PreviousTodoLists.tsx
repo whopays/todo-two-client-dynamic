7abd6cff32595015953dab6bc56ebad450fed1e5
demns <demn_@tut.by>
2023-03-08 22:56:56 +0100

    Adds button to remove list from history

--- src/components/PreviousTodoLists/PreviousTodoLists.tsx
+++ src/components/PreviousTodoLists/PreviousTodoLists.tsx
@@ -1,16 +1,21 @@
 import { useRef, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Box from '@mui/material/Box';
 import Grow from '@mui/material/Grow';
 import Paper from '@mui/material/Paper';
 import Popper from '@mui/material/Popper';
 import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
+import Typography from '@mui/material/Typography';
 import HistoryIcon from '@mui/icons-material/History';
-import { getPreviousTodoLists } from './previousTodoList';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { getPreviousTodoLists, removeTodoList } from './previousTodoList';
 
 export default function PreviousTodoLists() {
-  const previousTodoLists = getPreviousTodoLists();
+  const [previousTodoLists, setPreviousTodoLists] = useState(
+    getPreviousTodoLists(),
+  );
 
   const [open, setOpen] = useState(false);
   const anchorRef = useRef(null);
@@ -66,36 +71,56 @@ export default function PreviousTodoLists() {
                 placement === 'bottom-start' ? 'left top' : 'left bottom',
             }}
           >
-            <Paper>
-              <ClickAwayListener onClickAway={handleClose}>
-                <MenuList
-                  autoFocusItem={open}
-                  id="composition-menu"
-                  aria-labelledby="composition-button"
-                  onKeyDown={handleListKeyDown}
-                >
-                  {previousTodoLists.map(
-                    (previousTodoList: { id: any; title: string }) => {
-                      return (
-                        <MenuItem
-                          key={previousTodoList.id}
-                          onClick={() => {
-                            window.history.pushState(
-                              null,
-                              '',
-                              `/${previousTodoList.id}`,
-                            );
-                            window.location.reload();
-                          }}
-                        >
-                          {previousTodoList.title || '✏️ Missing title'}
-                        </MenuItem>
-                      );
-                    },
-                  )}
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
+            <Box maxWidth={340} overflow="hidden">
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    {previousTodoLists.map(
+                      ({ id, title }: { id: any; title: string }) => {
+                        return (
+                          <MenuItem
+                            key={id}
+                            onClick={() => {
+                              window.history.pushState(null, '', `/${id}`);
+                              window.location.reload();
+                            }}
+                          >
+                            <Box
+                              display="flex"
+                              justifyContent="space-between"
+                              alignItems="center"
+                              width="100%"
+                            >
+                              <Typography
+                                textOverflow="ellipsis"
+                                overflow="hidden"
+                              >
+                                {title || '✏️ Missing title'}
+                              </Typography>
+                              <IconButton
+                                aria-label="delete previous list entry"
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  removeTodoList(id);
+                                  setPreviousTodoLists(getPreviousTodoLists());
+                                }}
+                              >
+                                <DeleteForeverIcon />
+                              </IconButton>
+                            </Box>
+                          </MenuItem>
+                        );
+                      },
+                    )}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Box>
           </Grow>
         )}
       </Popper>
